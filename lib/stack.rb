28bebf9a223ec46adc01e1eade261546fdfa97fe
Ashley Wong <tofudragon333@gmail.com>
2022-09-23 12:04:33 -0400

    commit tree f098b71f0bea21d6163faffb9fa79ab0f0666046

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,20 @@
 # your code here
+class Stack
+    def initialize
+        @stack = []
+    end
+
+    def push(value)
+        @stack.push(value)
+        # result = ""
+        # result = stack + result
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+end
